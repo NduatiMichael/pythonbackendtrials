2b39f9fb9244a2ec0669ac109feda708b1b59368
NduatiMichael <mknduati@gmail.com>
2024-03-16 23:10:56 +0300

    Data structures

---
+++ Learning/Python/data _structure.py
@@ -0,0 +1,36 @@
+# data types
+a = 2
+b = 3
+c = 3.4
+status = True
+name = "Virtuosi"
+
+#operators
+d = a + c
+e = a * b
+f = b / a
+g = b % a
+h = b ** a
+
+#list
+estates = ["Karen", "Roselyne","Muthaiga", "Runda"]
+print(estates)
+estates.append("Limuru")
+print(estates)
+estates.append("Gikambura")
+print(estates)
+estates.pop()
+print(estates)
+estates.remove("Runda")
+print(estates)
+print(estates[1])
+estates.sort()
+print (estates)
+estates[1] = "Limuru1"
+print(estates)
+
+#Tuple
+fare = (200,500,600)
+print (fare)
+
+#dictionaries
