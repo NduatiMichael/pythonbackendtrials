b4dd29c8ddccedd87bde10631d8a842709ee32cc
NduatiMichael <mknduati@gmail.com>
2024-03-20 17:32:07 +0300

    Auni api setup

---
+++ Learning/Python/auniapi.py
@@ -0,0 +1,67 @@
+
+"""
+Load  credit:
+1 - Validate the amount and phone number
+2 - Validate the telco to be used
+3 - Trigger Credit loading or return message
+
+"""
+
+
+"""
+Process credit loading
+1 - Trigger the relevant mobile money api
+2 - listen for the call back response
+3 - update the database if successful
+4 - Return a message
+
+"""
+
+
+"""
+validate top up:
+1 - Validate phone number length
+2 - Validate whether Telco is within the accepted list
+3 - Check if user's balance is sufficient to process the top up
+4 - Trigger credit disbersment or return a Message
+"""
+
+
+
+"""
+Process credit disbersment:
+1 - Trigger africastalking API
+2 - Listen for the call back
+3 - Update the database if successful
+4 - Return Message
+"""
+
+
+"""
+Bulk Disbersment:
+1 - find the sum of the credits to be topped up
+2 - Validate the account has enough balance to process the selected batch
+3 - Trigger the credit disbersment process
+4 - Return success or failed
+"""
+
+
+#Saraficom Daraja API
+
+
+
+#Airtel money API
+
+
+
+#Telkom Kenya API
+
+
+
+#Momo API
+
+
+
+#Vodacom API
+
+
